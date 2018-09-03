35d1754222c01cb8714383b91ec922b4675f795e
Gustavo Ogasawara <42575832+Guakio@users.noreply.github.com>
2018-09-03 14:34:40 -0300

    Assignment2
    Function working

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Use a function Solve Matrix with cache data
+## This is exactly the same of the previuous example cachemean but another 
+## function and class
 
-## Write a short comment describing this function
+## This is a function to make a square matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function solve the matrix and cached past results
 
 cacheSolve <- function(x, ...) {
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
+        
+
         ## Return a matrix that is the inverse of 'x'
 }
